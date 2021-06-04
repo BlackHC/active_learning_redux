5ca06ecb947fcb8772461045b8219949fcd79af3
Andreas Kirsch <blackhc@gmail.com>
2021-06-04 16:23:56 +0100

    Experiment results
    CU-hz40ef

---
+++ batchbald_redux/results/experiment_job_11_2021-06-04-021622.py
@@ -0,0 +1,93 @@
+store = {}
+store['timestamp']=1622769382
+store['cmdline']=['/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py', '--id=11']
+store['commit']='03f69f03c653152ecfa74b84289906c55ed08e71'
+store['github_url']='03f69f03c653152ecfa74b84289906c55ed08e71'
+store['experiment']='/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py'
+store['job_id']=11
+store['worker_id']=11
+store['num_workers']=60
+store['config']={'seed': 800, 'acquisition_size': 5, 'max_training_set': 300, 'num_pool_samples': 100, 'num_validation_samples': 20, 'num_training_samples': 1, 'num_patience_epochs': 20, 'max_training_epochs': 120, 'training_batch_size': 64, 'device': 'cuda', 'validation_set_size': 2048, 'initial_set_size': 20, 'min_samples_per_epoch': 1024, 'repeated_mnist_repetitions': 2, 'add_dataset_noise': False, 'acquisition_function': 'batchbald_redux.acquisition_functions.BatchEIG', 'train_eval_model': 'batchbald_redux.train_eval_model.TrainSelfDistillationEvalModel', 'model_optimizer_factory': 'batchbald_redux.models.MnistOptimizerFactory', 'acquisition_function_args': None, 'temperature': 0.0}
+store['log']={}
+store['initial_training_set_indices']=[38043, 40091, 17418, 2094, 39879, 3133, 5011, 40683, 54379, 24287, 9849, 59305, 39508, 39356, 8758, 52579, 13655, 7636, 21562, 41329]
+store['dataset_info']={'training': "'FastMNIST (Train)'x2", 'test': "'FastMNIST (Test)'"}
+store['active_learning_steps']=[]
+store['active_learning_steps'].append({})
+store['active_learning_steps'][0]['training']={}
+store['active_learning_steps'][0]['training']['epochs']=[]
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.4814453125, 'crossentropy': 1.9923837184906006})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6201171875, 'crossentropy': 1.7640353441238403})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63330078125, 'crossentropy': 2.09255313873291})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.65087890625, 'crossentropy': 2.27864146232605})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64404296875, 'crossentropy': 2.6002886295318604})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64453125, 'crossentropy': 2.722993850708008})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6484375, 'crossentropy': 2.7934348583221436})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63330078125, 'crossentropy': 2.859206438064575})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64208984375, 'crossentropy': 2.8782451152801514})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63623046875, 'crossentropy': 2.9576592445373535})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64990234375, 'crossentropy': 3.0101470947265625})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6337890625, 'crossentropy': 3.1566507816314697})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6455078125, 'crossentropy': 2.9979355335235596})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.65283203125, 'crossentropy': 2.931245803833008})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.646484375, 'crossentropy': 3.134704351425171})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63916015625, 'crossentropy': 3.2411930561065674})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.650390625, 'crossentropy': 3.300265312194824})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64111328125, 'crossentropy': 3.295961380004883})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62548828125, 'crossentropy': 3.3353164196014404})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62353515625, 'crossentropy': 3.4384326934814453})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.61962890625, 'crossentropy': 3.5685038566589355})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62890625, 'crossentropy': 3.3612775802612305})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.65625, 'crossentropy': 3.15438175201416})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6552734375, 'crossentropy': 3.212343454360962})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6591796875, 'crossentropy': 3.3622255325317383})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64453125, 'crossentropy': 3.355297088623047})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63720703125, 'crossentropy': 3.4564669132232666})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6240234375, 'crossentropy': 3.5538649559020996})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6474609375, 'crossentropy': 3.438990592956543})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6484375, 'crossentropy': 3.4387173652648926})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64404296875, 'crossentropy': 3.452570915222168})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6484375, 'crossentropy': 3.401151418685913})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64501953125, 'crossentropy': 3.30727219581604})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.642578125, 'crossentropy': 3.5118393898010254})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63916015625, 'crossentropy': 3.6486029624938965})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6474609375, 'crossentropy': 3.57458233833313})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64404296875, 'crossentropy': 3.587068796157837})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.642578125, 'crossentropy': 3.5413243770599365})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6279296875, 'crossentropy': 3.7962493896484375})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63232421875, 'crossentropy': 3.8960318565368652})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63623046875, 'crossentropy': 3.6721372604370117})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64208984375, 'crossentropy': 3.4207749366760254})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6142578125, 'crossentropy': 3.5785019397735596})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64794921875, 'crossentropy': 3.13384747505188})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.64111328125, 'crossentropy': 3.157860517501831})
+store['active_learning_steps'][0]['training']['best_epoch']=25
+store['active_learning_steps'][0]['evaluation_metrics']={'accuracy': 0.6628, 'crossentropy': 3.42143359375}
+store['active_learning_steps'][0]['eval_training']={}
+store['active_learning_steps'][0]['eval_training']['epochs']=[]
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.3359375, 'crossentropy': 2.0783450603485107})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.59130859375, 'crossentropy': 1.5317070484161377})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.63671875, 'crossentropy': 1.3442145586013794})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.671875, 'crossentropy': 1.3512364625930786})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.67578125, 'crossentropy': 1.269347071647644})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6669921875, 'crossentropy': 1.3307435512542725})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6728515625, 'crossentropy': 1.278095006942749})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6796875, 'crossentropy': 1.2846919298171997})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.66845703125, 'crossentropy': 1.2832674980163574})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.66357421875, 'crossentropy': 1.2843619585037231})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.658203125, 'crossentropy': 1.3640114068984985})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.671875, 'crossentropy': 1.2974015474319458})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6650390625, 'crossentropy': 1.310060977935791})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.669921875, 'crossentropy': 1.3164142370224})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.66650390625, 'crossentropy': 1.2711913585662842})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.669921875, 'crossentropy': 1.2523750066757202})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.67041015625, 'crossentropy': 1.325904369354248})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.65576171875, 'crossentropy': 1.3869552612304688})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6669921875, 'crossentropy': 1.2486861944198608})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.66943359375, 'crossentropy': 1.3112304210662842})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6787109375, 'crossentropy': 1.2465280294418335})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6708984375, 'crossentropy': 1.2767951488494873})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.67333984375, 'crossentropy': 1.29787278175354})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6826171875, 'crossentropy': 1.2156569957733154})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6748046875, 'crossentropy': 1.2701992988586426})
+store['active_learning_steps'][0]['eval_training']['best_epoch']=24
+store['exception']='Traceback (most recent call last):\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py", line 264, in <module>\n    config.run(store=store)\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py", line 208, in run\n    candidate_batch = acquisition_function.compute_candidate_batch(\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/acquisition_functions.py", line 232, in compute_candidate_batch\n    return self.get_candidate_batch(log_probs_N_K_C, log_eval_probs_N_K_C, device)\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/acquisition_functions.py", line 313, in get_candidate_batch\n    num_samples=self.num_samples,\nAttributeError: \'BatchEIG\' object has no attribute \'num_samples\'\n'
---
+++ batchbald_redux/results/experiment_job_58_2021-06-04-095021.py
@@ -0,0 +1,59 @@
+store = {}
+store['timestamp']=1622796621
+store['cmdline']=['/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py', '--id=58']
+store['commit']='03f69f03c653152ecfa74b84289906c55ed08e71'
+store['github_url']='03f69f03c653152ecfa74b84289906c55ed08e71'
+store['experiment']='/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py'
+store['job_id']=58
+store['worker_id']=58
+store['num_workers']=60
+store['config']={'seed': 851, 'acquisition_size': 5, 'max_training_set': 300, 'num_pool_samples': 100, 'num_validation_samples': 20, 'num_training_samples': 1, 'num_patience_epochs': 20, 'max_training_epochs': 120, 'training_batch_size': 64, 'device': 'cuda', 'validation_set_size': 2048, 'initial_set_size': 20, 'min_samples_per_epoch': 1024, 'repeated_mnist_repetitions': 1, 'add_dataset_noise': False, 'acquisition_function': 'batchbald_redux.acquisition_functions.BatchEIG', 'train_eval_model': 'batchbald_redux.train_eval_model.TrainSelfDistillationEvalModel', 'model_optimizer_factory': 'batchbald_redux.models.MnistOptimizerFactory', 'acquisition_function_args': None, 'temperature': 0.0}
+store['log']={}
+store['initial_training_set_indices']=[38043, 40091, 17418, 2094, 39879, 3133, 5011, 40683, 54379, 24287, 9849, 59305, 39508, 39356, 8758, 52579, 13655, 7636, 21562, 41329]
+store['dataset_info']={'training': "'FastMNIST (Train)'", 'test': "'FastMNIST (Test)'"}
+store['active_learning_steps']=[]
+store['active_learning_steps'].append({})
+store['active_learning_steps'][0]['training']={}
+store['active_learning_steps'][0]['training']['epochs']=[]
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.4931640625, 'crossentropy': 2.0227231979370117})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.5791015625, 'crossentropy': 1.9028635025024414})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63818359375, 'crossentropy': 2.29085636138916})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63037109375, 'crossentropy': 2.559021234512329})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62841796875, 'crossentropy': 2.920403242111206})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63134765625, 'crossentropy': 2.9277002811431885})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63232421875, 'crossentropy': 3.0148041248321533})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.638671875, 'crossentropy': 3.1672515869140625})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6259765625, 'crossentropy': 3.3162972927093506})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62255859375, 'crossentropy': 3.3485116958618164})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62255859375, 'crossentropy': 3.467561960220337})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6123046875, 'crossentropy': 3.619023084640503})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62451171875, 'crossentropy': 3.6297786235809326})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6298828125, 'crossentropy': 3.521765947341919})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6376953125, 'crossentropy': 3.3686416149139404})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6103515625, 'crossentropy': 3.875458002090454})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6298828125, 'crossentropy': 3.5557477474212646})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.63818359375, 'crossentropy': 3.3677542209625244})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62890625, 'crossentropy': 3.5808446407318115})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.61865234375, 'crossentropy': 3.8125414848327637})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.61669921875, 'crossentropy': 3.833228588104248})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.6318359375, 'crossentropy': 3.592886209487915})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62841796875, 'crossentropy': 3.891542434692383})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62890625, 'crossentropy': 3.736384630203247})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62646484375, 'crossentropy': 3.8317739963531494})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.626953125, 'crossentropy': 3.7989768981933594})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.62255859375, 'crossentropy': 3.968644142150879})
+store['active_learning_steps'][0]['training']['epochs'].append({'accuracy': 0.61328125, 'crossentropy': 4.302202224731445})
+store['active_learning_steps'][0]['training']['best_epoch']=8
+store['active_learning_steps'][0]['evaluation_metrics']={'accuracy': 0.6199, 'crossentropy': 3.2735279296875}
+store['active_learning_steps'][0]['eval_training']={}
+store['active_learning_steps'][0]['eval_training']['epochs']=[]
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.33447265625, 'crossentropy': 2.012387275695801})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.5595703125, 'crossentropy': 1.5487436056137085})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.63671875, 'crossentropy': 1.3629848957061768})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.62353515625, 'crossentropy': 1.3953717947006226})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.62548828125, 'crossentropy': 1.4277172088623047})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.60546875, 'crossentropy': 1.4631444215774536})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6181640625, 'crossentropy': 1.3859943151474})
+store['active_learning_steps'][0]['eval_training']['epochs'].append({'accuracy': 0.6435546875, 'crossentropy': 1.3833714723587036})
+store['active_learning_steps'][0]['eval_training']['best_epoch']=8
+store['exception']='Traceback (most recent call last):\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py", line 264, in <module>\n    config.run(store=store)\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment.py", line 208, in run\n    candidate_batch = acquisition_function.compute_candidate_batch(\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/acquisition_functions.py", line 232, in compute_candidate_batch\n    return self.get_candidate_batch(log_probs_N_K_C, log_eval_probs_N_K_C, device)\n  File "/auto/users/andsch/github/active_learning_redux/batchbald_redux/acquisition_functions.py", line 313, in get_candidate_batch\n    num_samples=self.num_samples,\nAttributeError: \'BatchEIG\' object has no attribute \'num_samples\'\n'
