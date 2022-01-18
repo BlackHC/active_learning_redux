ab4cee0a0cc35c90e973cc1e997363deb69efdb3
Andreas Kirsch <blackhc@gmail.com>
2022-01-18 11:16:20 +0000

    Fix config :( and add more exps
    CU-1uhfjwr

--- batchbald_redux/experiment_repeated_mnist.py
+++ batchbald_redux/experiment_repeated_mnist.py
@@ -47,7 +47,8 @@ configs = [
     for acquisition_size in [10]
     for num_pool_samples in [100]
     for id_repetitions in [1,2,4]
-    for stochastic_mode in [acquisition_functions.StochasticMode.Power]
+    # Already ran Power
+    for stochastic_mode in [acquisition_functions.StochasticMode.Softmax, acquisition_functions.StochasticMode.Softrank]
 ] + [
     UnifiedExperiment(
         experiment_data_config=StandardExperimentDataConfig(
@@ -64,7 +65,7 @@ configs = [
         acquisition_function=acquisition_function,
         acquisition_size=acquisition_size,
         num_pool_samples=num_pool_samples,
-        max_training_set=120,
+        max_training_set=300,
         model_trainer_factory=MnistModelTrainer,
     )
     for acquisition_function in [
@@ -76,7 +77,34 @@ configs = [
     for acquisition_size in [10]
     for num_pool_samples in [100]
     for id_repetitions in [1,2,4]
+] + [
+    UnifiedExperiment(
+        experiment_data_config=StandardExperimentDataConfig(
+            id_dataset_name="MNIST",
+            id_repetitions=id_repetitions,
+            initial_training_set_size=20,
+            validation_set_size=4096,
+            validation_split_random_state=0,
+            evaluation_set_size=0,
+            add_dataset_noise=True,
+            ood_dataset_config=None,
+        ),
+        seed=seed + 8945,
+        acquisition_function=acquisition_function,
+        acquisition_size=acquisition_size,
+        num_pool_samples=num_pool_samples,
+        max_training_set=300,
+        model_trainer_factory=MnistModelTrainer,
+    )
+    for acquisition_function in [
+        baseline_acquisition_functions.BADGE
+    ]
+    for seed in range(5)
+    for acquisition_size in [20, 40]
+    for num_pool_samples in [100]
+    for id_repetitions in [1,2,4]
 ]
+
 
 if not is_run_from_ipython() and __name__ == "__main__":
     for job_id, store in embedded_experiments(__file__, len(configs)):
