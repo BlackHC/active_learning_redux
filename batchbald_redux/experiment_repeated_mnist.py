3b54083bc2a9c5fe5097b77f09123aa044b49f50
Andreas Kirsch <blackhc@gmail.com>
2022-01-20 00:27:27 +0000

    Train with temp 8 and 1 (instead of coldness 8), so we can show an ablation.
    CU-1uhfjwr

--- batchbald_redux/experiment_repeated_mnist.py
+++ batchbald_redux/experiment_repeated_mnist.py
@@ -10,12 +10,10 @@ import traceback
 from blackhc.project import is_run_from_ipython
 from blackhc.project.experiment import embedded_experiments
 
-from batchbald_redux import acquisition_functions
-from batchbald_redux import baseline_acquisition_functions
-from .unified_experiment import UnifiedExperiment
+from batchbald_redux import acquisition_functions, baseline_acquisition_functions
 from .experiment_data import StandardExperimentDataConfig
-
 from .models import MnistModelTrainer
+from .unified_experiment import UnifiedExperiment
 
 # Cell
 
@@ -37,15 +35,51 @@ configs = [
         num_pool_samples=num_pool_samples,
         max_training_set=300,
         model_trainer_factory=MnistModelTrainer,
+        stochastic_mode=stochastic_mode,
+        coldness=coldness,
     )
     for acquisition_function in [
-        acquisition_functions.BatchBALD,
+        acquisition_functions.StochasticBALD,
     ]
     for seed in range(5)
-    for acquisition_size in [5]
+    for acquisition_size in [10]
     for num_pool_samples in [100]
-    for id_repetitions in [1,2,4]
-] #+ [
+    for id_repetitions in [1, 2, 4]
+    for coldness in [1., 1/8.]
+    # Already ran Power
+    for stochastic_mode in [
+        acquisition_functions.StochasticMode.Power,
+        acquisition_functions.StochasticMode.Softmax,
+        acquisition_functions.StochasticMode.Softrank,
+    ]
+]
+# [
+#     UnifiedExperiment(
+#         experiment_data_config=StandardExperimentDataConfig(
+#             id_dataset_name="MNIST",
+#             id_repetitions=id_repetitions,
+#             initial_training_set_size=20,
+#             validation_set_size=4096,
+#             validation_split_random_state=0,
+#             evaluation_set_size=0,
+#             add_dataset_noise=True,
+#             ood_dataset_config=None,
+#         ),
+#         seed=seed + 8945,
+#         acquisition_function=acquisition_function,
+#         acquisition_size=acquisition_size,
+#         num_pool_samples=num_pool_samples,
+#         max_training_set=300,
+#         model_trainer_factory=MnistModelTrainer,
+#     )
+#     for acquisition_function in [
+#         acquisition_functions.BatchBALD,
+#     ]
+#     for seed in range(5)
+#     for acquisition_size in [5]
+#     for num_pool_samples in [100]
+#     for id_repetitions in [1,2,4]
+# ] + [
 #     UnifiedExperiment(
 #         experiment_data_config=StandardExperimentDataConfig(
 #             id_dataset_name="MNIST",
