b24604a1a2abfaf0148b39f35b3015d605eef3cd
Andreas Kirsch <blackhc@gmail.com>
2022-01-25 23:34:51 +0000

    New RMNIST experiments
    CU-1uhfjwr

--- batchbald_redux/experiment_repeated_mnist.py
+++ batchbald_redux/experiment_repeated_mnist.py
@@ -29,33 +29,7 @@ configs = [
             add_dataset_noise=True,
             ood_dataset_config=None,
         ),
-        seed=seed + 8945,
-        acquisition_function=acquisition_function,
-        acquisition_size=acquisition_size,
-        num_pool_samples=num_pool_samples,
-        max_training_set=300,
-        model_trainer_factory=MnistModelTrainer,
-    )
-    for acquisition_function in [
-        baseline_acquisition_functions.BADGE
-    ]
-    for seed in range(5)
-    for acquisition_size in [80,160]
-    for num_pool_samples in [100]
-    for id_repetitions in [1,2,4]
-] + [
-    UnifiedExperiment(
-        experiment_data_config=StandardExperimentDataConfig(
-            id_dataset_name="MNIST",
-            id_repetitions=id_repetitions,
-            initial_training_set_size=20,
-            validation_set_size=4096,
-            validation_split_random_state=0,
-            evaluation_set_size=0,
-            add_dataset_noise=True,
-            ood_dataset_config=None,
-        ),
-        seed=seed + 8945,
+        seed=seed + 1765,
         acquisition_function=acquisition_function,
         acquisition_size=acquisition_size,
         num_pool_samples=num_pool_samples,
@@ -68,10 +42,10 @@ configs = [
         acquisition_functions.StochasticBALD,
     ]
     for seed in range(5)
-    for acquisition_size in [10]
+    for acquisition_size in [20, 40]
     for num_pool_samples in [100]
     for id_repetitions in [1, 2, 4]
-    for coldness in [16, 4.]
+    for coldness in [8, 4, 1]
     # Already ran Power
     for stochastic_mode in [
         acquisition_functions.StochasticMode.Power,
@@ -89,7 +63,7 @@ configs = [
             add_dataset_noise=True,
             ood_dataset_config=None,
         ),
-        seed=seed + 8945,
+        seed=seed + 1765,
         acquisition_function=acquisition_function,
         acquisition_size=acquisition_size,
         num_pool_samples=num_pool_samples,
@@ -102,14 +76,40 @@ configs = [
         acquisition_functions.StochasticBALD,
     ]
     for seed in range(5)
-    for acquisition_size in [10]
+    for acquisition_size in [20, 40]
     for num_pool_samples in [100]
     for id_repetitions in [1, 2, 4]
-    for coldness in [4, 1/4.]
+    for coldness in [1.]
     # Already ran Power
     for stochastic_mode in [
         acquisition_functions.StochasticMode.Softrank,
     ]
+] + [
+    UnifiedExperiment(
+        experiment_data_config=StandardExperimentDataConfig(
+            id_dataset_name="MNIST",
+            id_repetitions=id_repetitions,
+            initial_training_set_size=20,
+            validation_set_size=4096,
+            validation_split_random_state=0,
+            evaluation_set_size=0,
+            add_dataset_noise=True,
+            ood_dataset_config=None,
+        ),
+        seed=seed + 1765,
+        acquisition_function=acquisition_function,
+        acquisition_size=acquisition_size,
+        num_pool_samples=num_pool_samples,
+        max_training_set=300,
+        model_trainer_factory=MnistModelTrainer,
+    )
+    for acquisition_function in [
+        acquisition_functions.BALD,
+    ]
+    for seed in range(5)
+    for acquisition_size in [20, 40]
+    for num_pool_samples in [100]
+    for id_repetitions in [1,2,4]
 ]
 # [
 #     UnifiedExperiment(
