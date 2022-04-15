9902ec04f6cc0e75e9af8574d140f3f45a54778b
Andreas Kirsch <blackhc@gmail.com>
2022-04-15 15:47:46 +0100

    Add EMNIST results
    CU-26pg7f9

---
+++ batchbald_redux/results/experiment_emnist_job_139_2022-04-11-213732.py
@@ -0,0 +1,19 @@
+store = {}
+store['timestamp']=1649709452
+store['cmdline']=['/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment_emnist.py', '--id=139']
+store['commit']='e473826fad676d39f3cef13eb6dcb22d0ea7d982'
+store['github_url']='e473826fad676d39f3cef13eb6dcb22d0ea7d982'
+store['experiment']='/auto/users/andsch/github/active_learning_redux/batchbald_redux/experiment_emnist.py'
+store['job_id']=139
+store['worker_id']=139
+store['num_workers']=140
+store['config']={'seed': 1908, 'experiment_data_config': {'id_dataset_name': 'BalancedEMNIST', 'id_repetitions': 1, 'initial_training_set_size': 0, 'validation_set_size': 4096, 'validation_split_random_state': 0, 'evaluation_set_size': 0, 'add_dataset_noise': False, 'ood_dataset_config': None}, 'acquisition_size': 5, 'max_training_set': 300, 'max_training_epochs': 300, 'num_pool_samples': 20, 'num_validation_samples': 20, 'num_training_samples': 1, 'device': 'cuda', 'acquisition_function': 'batchbald_redux.acquisition_functions.BatchBALD', 'train_eval_model': 'batchbald_redux.train_eval_model.TrainSelfDistillationEvalModel', 'model_trainer_factory': 'batchbald_redux.emnist_models.EMnistModelTrainer', 'ensemble_size': 1, 'temperature': 1.0, 'coldness': 1.0, 'stochastic_mode': None, 'epig_bootstrap_type': 'BootstrapType.NO_BOOTSTRAP', 'epig_bootstrap_factor': 1.0, 'epig_dtype': torch.float64, 'disable_training_augmentations': False, 'cache_explicit_eval_model': False, 'resnet18_dropout_head': True}
+store['log']={}
+store['dataset_info']={'training': "'EMNIST (Train, seed=0, 108704 samples)'", 'test': "'EMNIST (Test)'"}
+store['initial_training_set_indices']=[]
+store['evaluation_set_indices']=[]
+store['seed']=17558245050011664244
+store['active_learning_steps']=[]
+store['active_learning_steps'].append({})
+store['active_learning_steps'][0]['training']={}
+store['active_learning_steps'][0]['evaluation_metrics']={'accuracy': 0.01851063829787234, 'crossentropy': 3.8588099479675293}
